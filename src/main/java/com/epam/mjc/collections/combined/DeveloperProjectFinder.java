d2c8253774508a3294b117e37575109ed4be1fd9
azimjonalijonov <115147667+azimjonalijonov@users.noreply.github.com>
2023-04-29 20:50:51 +0500

    Update DeveloperProjectFinder.java

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,13 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> strings = new ArrayList<>();
+        return strings;
     }
 }
