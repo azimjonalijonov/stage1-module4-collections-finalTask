6e2ef7d18af5095c93d80b8e29016967e6b5ccdd
azimjonalijonov <115147667+azimjonalijonov@users.noreply.github.com>
2023-04-29 20:51:44 +0500

    Update LessonsGetter.java

--- src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
+++ src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
@@ -1,10 +1,21 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class LessonsGetter {
     public Set<String> getLessons(Map<String, List<String>> timetable) {
+        Set<String> strings = new HashSet<>();
+        String[] weekDays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        for (String e : weekDays) {
+            if (timetable.containsKey(e)) {
+                for (String str : timetable.get(e)) {
+                    strings.add(str);
+                }
+            }
+        }
+        return strings;
     }
 }
